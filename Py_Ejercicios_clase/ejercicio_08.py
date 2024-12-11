0a57b389efa3a8f8fc86f4c2230cf68a8d3602a4
_-=Randomazzo=-_ <115214454+ibarraja@users.noreply.github.com>
2024-12-11 09:52:01 +0100

    Update ejercicio_08.py

--- Py_Ejercicios_clase/ejercicio_08.py
+++ Py_Ejercicios_clase/ejercicio_08.py
@@ -1,1 +1,35 @@
-...
+# Almacena la siguiente información sobre los álbumes más vendidos en los años 90 en una lista de diccionarios (menos mal que no existía el Reggaeton):
+# Recuerda que cada álbum es un diccionario.
+# (Artista o grupo, álbum, unidades vendidas y país del artista)
+
+# 1993 Gloria Estefan Mi tierra 1.000.000  Cuba
+# 1993 Laura Pausini Laura Pausini 1.350.000  Italia
+# 1991 Juan Luis Guerra Bachata Rosa 700.000  República Dominicana
+# 2002 Operación Triunfo Álbum 1.200.000  España
+# 1996 Spice Girls Spice 1.000.000  Reino Unido
+# 2000 La Oreja de Van Gogh El viaje de Copperpot 1.100.000  España
+# 1991 Mecano Aidalai 1.000.000  España
+# 1997 Luis Miguel Romances 800.000  México
+# 2000 Alejandro Sanz El alma al aire 1.300.000  España
+# 2002 David Bisbal Corazón latino 1.300.000  España
+# 1985 Rocío Jurado Paloma Brava 700.000  España
+# 1997 Alejandro Sanz Más 2.200.000  España
+# 1990 Julio Iglesias Raíces 1.100.000  España
+# 1996 Rosana Lunas Rotas 1.100.000  España
+# 1998 Chayanne Atado a tu amor 1.000.000  Puerto Rico
+# 1993 Abba ABBA Gold 800.000  Suecia
+# 2001 Álex Ubago ¿Qué pides tú? 900.000  España
+# 1988 Mecano Descanso Dominical 1.100.000  España
+# 2002 Amaral Estrella de mar 800.000  España
+# 1991 Alejandro Sanz Viviendo deprisa 800.000  España
+# 1995 Alejandro Sanz 3 800.000  España
+# 1997 Backstreet Boys Backstreet's Back 800.000  Reino Unido
+# 1996 Ella baila sola Ella baila sola 700.000  España
+# 1999 Estopa Estopa  1.100.000  España
+# 1997 Mónica Naranjo Palabra de Mujer 1.000.000  España
+# 1981 Julio Iglesias De niña a mujer 700.000  España
+# 1982 Michael Jackson Thriller 700.000  Estados Unidos
+# 1989 Phil Collins ...But Seriously 700.000  Reino Unido
+# 1979 Rocío Jurado Señora 900.000  España
+# 2003 Alejandro Sanz No es lo mismo 800.000  España
+# 2004 David Bisbal Bulería 1.000.000  España
