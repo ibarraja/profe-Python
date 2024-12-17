5f454481eb9aaae7c9a55a6ab97e72b97a15ec8a
_-=Randomazzo=-_ <115214454+ibarraja@users.noreply.github.com>
2024-12-17 12:14:51 +0100

    Create ejercicio_10.py

---
+++ Py_Ejercicios_clase/ejercicio_10.py
@@ -0,0 +1,26 @@
+# Calculadora con manejo de excepciones y menú interactivo
+# Crea un programa en Python que simule una calculadora interactiva. El programa debe cumplir con los siguientes requisitos:
+
+# Menú interactivo:
+
+#   El programa debe mostrar un menú con las siguientes opciones:
+#       1. Sumar dos números.
+#       2. Restar dos números.
+#       3. Multiplicar dos números.
+#       4. Dividir dos números.
+#       5. Salir del programa.
+
+#   Entrada del usuario:
+#       Los usuarios seleccionan una opción introduciendo el número correspondiente.
+#       El programa debe solicitar dos números para realizar la operación seleccionada.
+
+# Manejo de excepciones con try-except:
+
+#   Captura los siguientes errores:
+#       El usuario introduce un valor no numérico al seleccionar una opción o introducir números.
+#       Intento de división por cero.
+#       Selección de una opción fuera del rango permitido (1-5).
+
+# Comportamiento del programa:
+#   El programa debe continuar ejecutándose hasta que el usuario elija la opción 5 (Salir).
+#   Si ocurre un error, el programa debe mostrar un mensaje adecuado y permitir al usuario intentarlo de nuevo.
