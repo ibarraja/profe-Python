d06e4a16cf9a836df0f87c73617c51cff35354de
ibarraja <javier.ibarra.mz@gmail.com>
2025-01-09 20:04:28 +0100

    ejercicio 11 resuelto

--- Py_Ejercicios_clase/ejercicio_11-numeroPrimo.py
+++ Py_Ejercicios_clase/ejercicio_11-numeroPrimo.py
@@ -1,5 +1,3 @@
-# Ejercicio 11: Verificador de Números Primos
-# Enunciado:
 # Escribe un programa en Python que determine si un número entero dado es primo o no.
 #  -  Crea una función llamada es_primo(numero) que reciba un número entero como parámetro y devuelva True si el número es primo, o False en caso contrario.
 #  -  El programa debe pedir al usuario un número entero e indicar si es primo o no utilizando la función implementada.
@@ -10,16 +8,16 @@ def es_primo(num):
     contador = 1
     for i in range(num):
         contador += 1
-        if num % contador == 0 and contador != num:
+        if (num % contador == 0) and (contador != num):
             return False
     return True
 
 if __name__ == "__main__":
-    num=int(input("Dame un número: "))
-    if es_primo(num):
-        print(f"{num} es primo")
-    else:
-        print(f"{num} no es primo")
-        
-        
-    
+    try:
+        num=int(input("Dame un número: "))
+        if es_primo(num):
+            print(f"{num} es primo")
+        else:
+            print(f"{num} no es primo")
+    except ValueError as e:
+        print(f"Error: {e}")
