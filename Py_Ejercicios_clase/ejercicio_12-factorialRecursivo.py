9cccd82efdd43a5ed9d8f6244cabef2f4c478044
_-=Randomazzo=-_ <115214454+ibarraja@users.noreply.github.com>
2025-01-10 08:23:18 +0100

    Update ejercicio_12-factorialRecursivo.py

--- Py_Ejercicios_clase/ejercicio_12-factorialRecursivo.py
+++ Py_Ejercicios_clase/ejercicio_12-factorialRecursivo.py
@@ -1,7 +1,8 @@
 # Escribe un programa en Python que calcule el factorial de un número entero no negativo utilizando recursividad.
 #  -  Crea una función llamada factorial(n) que reciba un número entero como parámetro y devuelva su factorial.
 #  -  El programa debe pedir al usuario un número entero positivo y mostrar su factorial calculado mediante la función implementada.
-#  -  Asegúrate de manejar correctamente el caso base para la recursión.
+#  -  Un factorial, por ejemplo de 5, seria del siguiente modo:
+#      - 5! = 5*4*3*2*1
 
 def factorial(num):
 
