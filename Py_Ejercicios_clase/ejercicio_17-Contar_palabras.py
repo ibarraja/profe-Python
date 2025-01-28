09dc57ab82c817779197d58a647ff6a131c3a319
ibarraja <javier.ibarra.mz@gmail.com>
2025-01-28 10:52:52 +0100

    create POO exercises

--- Py_Ejercicios_clase/ejercicio_17-Contar_palabras.py
+++ Py_Ejercicios_clase/ejercicio_17-Contar_palabras.py
@@ -9,3 +9,32 @@
 # Crea un menú: 
 #   - opción 1: la cadena de texto es un input
 #   - opcion 2: la cadena de texto es una aleatoria de un Diccionario que tendreis que crear con al menos 6 frases. 
+
+def obtener_lista_palabras(c:str):
+    """
+    Recoge una cadena de texto y devuelve una lista con las palabras de la frase (En esta versión permite controlar '.')
+    """
+    # Creo una lista con las palabras de la frase que recojo.
+    lista = c.split(" ")
+    print(lista)
+    
+    i = 0
+    for item in lista:
+        
+        if item.endswith("."):    
+            lista[i] = item[:-1]
+        i += 1
+    
+    return lista
+    
+def mostrar_menu():
+    """
+    Muestra un menu con la información de lo que podemos hacer
+    """
+    
+    
+
+if __name__ == "__main__":
+    mostrar_menu()
+    cadena = "Hola mundo. El mundo es increíble. Hola de nuevo."
+    obtener_lista_palabras(cadena)
